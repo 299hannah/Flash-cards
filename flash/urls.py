1638cad3b4eb986b5986f5e60044fcfb9090e3be
obewas <obewas1202@gmail.com>
2021-07-21 20:49:06 +0300

    created task views

--- flash/urls.py
+++ flash/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
 
-path('', views.index, name='homepage')
+path('', views.index, name='homepage'),
+path('update/<int:pk>/', views.update_task, name='update'),
+path('create', views.create_task, name='create'),
+path('detail<int:pk>', views.get_task, name='detail'),
+path('delete/<int:pk>/', delete_task, name='delete')
+
 ]
